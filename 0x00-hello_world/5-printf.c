b2f51028fbf2be4a6c4dfbe33be70cc3f065657c
abrehamzdo <abrehamzewdu231994@gmail.com>
2022-12-11 11:14:44 -0800

    some changes on task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+
+
 /**
  * main - Entry point
- * *Return: Always 0 (success)
+ *
+ * Return: Always 0 (success)
  */
+
 int main(void)
-{ 
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0).
-}
+{
+        printf("\"programming is like building multilingual puzzle");
+        return (0);
+
+} 
+
