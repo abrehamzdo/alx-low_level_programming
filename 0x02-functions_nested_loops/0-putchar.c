7b45d728bd150d44c2dd0906bc39a06d10b8f4c4
abrehamzdo <abrehamzewdu231994@gmail.com>
2022-12-14 07:19:42 -0800

    updated Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,10 @@
 #include "main.h"
-
-
 /**
  *main - Entry point
  *
  *Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 	_putchar(' ');
         _putchar('p');
@@ -20,6 +16,6 @@ int main(void)
         _putchar('r');
         _putchar('/n');
 
-        return (0);
+  return (0);
 }
 
