5463cf1a1ccd24dfcee1dd53cf31d79caf5611e8
abrehamzdo <abrehamzewdu231994@gmail.com>
2023-02-16 00:48:05 -0800

    some changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
 /**
- *main - Entry point
- *
- *Return: Always 0 (Success)
+ *main-Entrypoint 
+ *Return:Always 0 (success)
  */
 int main(void)
 {
-	_putchar(' ');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('o');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('/n');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-  return (0);
+	return (0);
 }
 
