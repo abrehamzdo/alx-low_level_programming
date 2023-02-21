8010bd633b0e215a7ed27437dcfe339af464604e
abrehamzdo <117824333+abrehamzdo@users.noreply.github.com>
2023-02-22 02:06:41 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-
-
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-printf("\"programming is like building multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-
-} 
+}
 
